2b924b7bfd9aca17c988e3d268fcc14aecbfcaa4
Vladislav <vladozdeveloper@gmail.com>
2022-11-09 23:11:16 +0300

    created createContext and Cookie

---
+++ app/services/auth/auth.helper.ts
@@ -0,0 +1,20 @@
+export interface IAuthData{
+    user:{
+        _id:string
+        email:string
+    } | null
+    accessToken:string
+}
+
+export const saveTokenToStorage=(accessToken:string)=>{
+    Cookies.set('accessToken', accessToken)
+}
+
+export const removeTokenFromStorage=()=>{
+    Cookies.remove('accessToken')
+}
+
+export const saveToStorage=(data:IAuthData)=>{
+    saveTokenToStorage(data.accessToken)
+    localStorage.setItem('user',JSON.stringify(data.user))
+}
