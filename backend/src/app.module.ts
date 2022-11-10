a0d55574fb1d4f2a43cc22484844ce67b597f38c
Vladislav <vladozdeveloper@gmail.com>
2022-11-10 22:20:27 +0300

    created configservice and typegoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypegooseModule} from 'nestjs-typegoose';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import { getMongoConfig } from './config/mongo.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:getMongoConfig,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
