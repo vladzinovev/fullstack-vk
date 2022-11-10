00f843ee70ff5d3840a1b55aea48e6d612b8c423
Vladislav <vladozdeveloper@gmail.com>
2022-11-10 21:36:34 +0300

    created google autorization

---
+++ backend/src/auth/strategies/google.strategy.ts
@@ -0,0 +1,35 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { ConfigService } from '@nestjs/config';
+
+
+import { Injectable } from '@nestjs/common';
+
+
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+
+  constructor(
+    private readonly configService: ConfigService
+  ) {
+    super({
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_SECRET'),
+      callbackURL: 'http://localhost:3000/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate (accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { name, emails, photos } = profile
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken
+    }
+    done(null, user);
+  }
+}
