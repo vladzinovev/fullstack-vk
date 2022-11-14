4d1b3ebdf8dbd74fc1519f092311787f02fcb57c
Vladislav <vladozdeveloper@gmail.com>
2022-11-14 14:18:31 +0300

    created jwt strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { GoogleStrategy } from '../google/google.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from 'src/user/user.model';
@@ -9,14 +8,7 @@ import { UserModel } from 'src/user/user.model';
 @Module({
   controllers: [AuthController],
 
-  providers: [
-    AuthService, 
-    GoogleStrategy,
-    {
-      provide: 'AUTH_SERVICE',
-      useClass: AuthService,
-    },
-  ],
+  providers: [AuthService,],
 
   imports: [
     ConfigModule, 
@@ -28,13 +20,6 @@ import { UserModel } from 'src/user/user.model';
         }
       }
     ])
-  ],
-
-  exports: [
-    {
-      provide: 'AUTH_SERVICE',
-      useClass: AuthService,
-    },
   ],
 })
 export class AuthModule {}
