1f560449a9e62da8c30d54a1b77675e548fa4d2c
Vladislav <vladozdeveloper@gmail.com>
2022-11-16 00:30:31 +0300

    created service and controller for log likes

--- backend/src/log-likes/log-likes.controller.ts
+++ backend/src/log-likes/log-likes.controller.ts
@@ -1,4 +1,46 @@
+import { Controller, Delete, Get, HttpCode, Param, Post } from "@nestjs/common";
+import { Types } from "mongoose";
+import { Auth } from "src/auth/auth.decorators";
+import { IdValidationPipe } from "src/pipes/id.validation.pipe";
+import { CurrentUser } from "src/user/decorators/user.decorator";
+import { LogLikesService } from "./log-likes.service";
+
 @Controller('comment')
 export class LogLikesController{
+    LogLikesService: any;
     constructor(private readonly commentService: LogLikesService){}
+
+    @Get('check-exists/:postId')
+    @Auth()
+    async checkExists(
+        @Param('postId',IdValidationPipe) postId:Types.ObjectId,
+        @CurrentUser('_id') userId: Types.ObjectId
+    ){
+        return this.LogLikesService.checkExists(userId,postId);
+    }
+
+    @Get('get-count/:postId')
+    async getAllCount(@Param('postId',IdValidationPipe) postId:Types.ObjectId){
+        return this.LogLikesService.getAllCount(postId);
+    }
+
+    @HttpCode(200)
+    @Post(':postId')
+    @Auth()
+    async createLog(
+        @Param('postId',IdValidationPipe) postId:Types.ObjectId,
+        @CurrentUser('_id') userId:Types.ObjectId
+    ){
+        return this.LogLikesService.create(userId,postId)
+    }
+
+    @HttpCode(200)
+    @Delete(':postId')
+    @Auth()
+    async deleteLog(
+        @Param('postId',IdValidationPipe) postId:Types.ObjectId,
+        @CurrentUser('_id') userId:Types.ObjectId
+    ){
+        return this.commentService.create(userId,postId)
+    }
 }
---
+++ backend/src/log-likes/log-likes.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+import { Types } from "mongoose";
+import { InjectModel } from "nestjs-typegoose";
+import { ModelType } from "typegoose";
+import { LogLikesModel } from "./log-likes.model";
+
+@Injectable()
+export class LogLikesService{
+    constructor(
+        @InjectModel(LogLikesModel) 
+        private readonly LogLikesModel:ModelType<LogLikesModel>
+    ) {}
+
+    async checkExists( userId:Types.ObjectId, postId:Types.ObjectId){
+        return this.LogLikesModel.exists({post:postId, user:userId})
+        .exec()
+    }
+
+    async getAllCount( postId:Types.ObjectId){
+        return this.LogLikesModel.find({post:postId})
+        .count()
+        .exec()
+    }
+
+    async create(userId:Types.ObjectId, postId:Types.ObjectId){
+        return this.LogLikesModel.create({post:postId, user:userId})
+    }
+
+    async delete(userId:Types.ObjectId, postId:Types.ObjectId){
+        return this.LogLikesModel.findOneAndDelete({post:postId, user:userId})
+        .exec()
+    }
+}
