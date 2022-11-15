d5a63599309c64fc1d7ab5b12b2b30c6cd65a0de
Vladislav <vladozdeveloper@gmail.com>
2022-11-15 21:11:47 +0300

    created all the necessary components through nest

---
+++ backend/src/log-likes/log-likes.model.ts
@@ -0,0 +1,33 @@
+import { prop, Ref } from '@typegoose/typegoose';
+import {Base, TimeStamps} from '@typegoose/typegoose/lib/defaultClasses';
+import { EnumGender } from 'src/user/user.interface';
+
+export interface PostModel extends Base{}
+
+export class PostModel extends TimeStamps{
+
+    @prop({unique:true})
+    email:string
+
+    @prop()
+    name:string
+
+    @prop({default:false})
+    isVerified:boolean
+
+    @prop()
+    birthDate:string
+
+    @prop()
+    city:string
+    
+    @prop({enum: EnumGender})
+    gender:string
+
+    @prop()
+    avatarPath:string
+
+    @prop({default:[], ref:()=>PostModel})
+    friends:Ref<PostModel>[]
+
+}
