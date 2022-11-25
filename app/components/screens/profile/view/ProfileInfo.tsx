ad738b9ff4f8e381d663b9e5e6a0d57a71c16240
Vladislav <vladozdeveloper@gmail.com>
2022-11-26 01:30:15 +0300

    added converter numWord (friend to friends)

--- app/components/screens/profile/view/ProfileInfo.tsx
+++ app/components/screens/profile/view/ProfileInfo.tsx
@@ -1,7 +1,8 @@
 import { IUser } from "@/types/user.interface"
-import { Button, Card, Typography } from "antd"
+import { Button, Card, Col, Row, Typography } from "antd"
 import { FC } from "react"
 import Image from "next/image"
+import { numWord } from "@/components/utils/numWord"
 
 const ProfileInfo:FC<{profile:IUser}>=({profile})=>{
     return(
@@ -9,6 +10,14 @@ const ProfileInfo:FC<{profile:IUser}>=({profile})=>{
             <Typography.Title level={1}>
                 {profile.name}
             </Typography.Title>
+
+            <Row gutter={[15,15]}>
+                <Col span={4}>
+                    <b>{profile.friends?.length || 0}</b>
+                    <div>{numWord(profile.friends?.length || 0,['друг','друга','друзей'])}</div>
+                </Col>
+            </Row>
+
         </Card>
     )
 }
