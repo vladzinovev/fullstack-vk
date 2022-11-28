e2c5eea105bb3bab76b067021b5c202a751e1377
Vladislav <vladozdeveloper@gmail.com>
2022-11-28 23:58:35 +0300

    added avatar editing

--- app/components/screens/profile/edit/ProfileEdit.tsx
+++ app/components/screens/profile/edit/ProfileEdit.tsx
@@ -5,16 +5,19 @@ import { IMediaResponse } from "@/services/media.service";
 import { UserService } from "@/services/user.service";
 import { IUserFields } from "@/types/user.interface";
 import { UploadOutlined } from "@ant-design/icons";
-import { Button, Card, Form, Typography, DatePicker, Select, Input, Image, notification  } from "antd";
+import { Button, Card, Form, Typography, DatePicker, Select, Input, Image, notification, Skeleton, Avatar  } from "antd";
 import { errorCatch } from "api/api.utils";
 import { FC, useState } from "react"
 import { useMutation } from "react-query";
+import moment from 'moment'
+import 'moment/locale/ru'
+import locale from 'antd/lib/locale/ru_RU'
 
 const ProfileEdit:FC=()=>{
 
     const [image, setImage] =useState<IMediaResponse>({} as IMediaResponse)
 
-    useProfile(({data})=>{
+    const {isLoading,data}=useProfile(({data})=>{
         setImage({
             name:'default',
             url:data.avatarPath
@@ -40,49 +43,53 @@ const ProfileEdit:FC=()=>{
 
     return (
         <Layout title='Редактирование'>
-            <Card  bodyStyle={{padding:'30px 0'}}>
+            <Card  bodyStyle={{marginTop:'1rem'}}>
                 <Typography.Title level={1} style={{textAlign:'center'}}>
                     Редактирование профиля
                 </Typography.Title>
-                <Form
-                    labelCol={{ span: 4 }}
-                    wrapperCol={{ span: 14 }}
-                    layout="horizontal"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                >
+                {isLoading ? (
+                    <Skeleton/>
+                ) :(
+                    <Form
+                        labelCol={{ span: 2 }}
+                        wrapperCol={{ span: 5 }}
+                        layout="horizontal"
+                        initialValues={data ?{...data, birthDate:moment(data.birthDate,'DD.MM.YYYY')} : {}}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                    >
 
-                    <Form.Item label="Имя" rules={[{required:true, message:'Пожалуйста укажите имя!'}]}>
-                        <Input placeholder='Введите имя'/>
-                    </Form.Item>
+                        <Form.Item name='name' label="Имя" rules={[{required:true, message:'Пожалуйста укажите имя!'}]}>
+                            <Input placeholder='Введите имя'/>
+                        </Form.Item>
 
-                    <Form.Item label="Город" rules={[{required:true, message:'Пожалуйста укажите город!'}]}>
-                        <Input placeholder='Введите город'/>
-                    </Form.Item>
+                        <Form.Item name='city' label="Город" rules={[{required:true, message:'Пожалуйста укажите город!'}]}>
+                            <Input placeholder='Введите город'/>
+                        </Form.Item>
 
-                    <Form.Item label="Дата рождения" rules={[{required:true, message:'Пожалуйста укажите дату рождения!'}]}>
-                        <DatePicker />
-                    </Form.Item>
+                        <Form.Item name='birthDate' label="Дата рождения" rules={[{required:true, message:'Пожалуйста укажите дату рождения!'}]}>
+                            
+                            <DatePicker format='DD.MM.YYYY'/>
+                        </Form.Item>
 
-                    <Form.Item label="Пол" rules={[{required:true, message:'Пожалуйста укажите пол!'}]}>
-                        <Select>
-                            <Select.Option value="male">мужской</Select.Option>
-                            <Select.Option value="female">женский</Select.Option>
-                        </Select>
-                    </Form.Item>
+                        <Form.Item name='gender' label="Пол" rules={[{required:true, message:'Пожалуйста укажите пол!'}]}>
+                            <Select>
+                                <Select.Option value="male">мужской</Select.Option>
+                                <Select.Option value="female">женский</Select.Option>
+                            </Select>
+                        </Form.Item>
 
-                    <div>
-                        <UploadField onChange={setImage} Button={<Button icon={<UploadOutlined/>}>Нажмите для загрузки</Button>}/>
-                        {image?.url && <Image src={image.url} alt={image.name}/>}
-                    </div>
-
-                    <UploadField onChange={setImage} Button={<Button icon={<UploadOutlined/>}>Обновите профиль</Button>}/>
-                    
-                    <Form.Item>
-                        <Button>Button</Button>
-                    </Form.Item>
-                </Form>
+                        <Form.Item>
+                            <UploadField onChange={setImage} Button={<Button icon={<UploadOutlined/>}>Нажмите для загрузки</Button>}/>
+                            {image?.url && <Avatar src={image.url} alt={image.name} size={120} style={{marginBottom:10}}/>}
+                        </Form.Item>
+                        
+                        <Form.Item>
+                            <Button type='primary' htmlType="submit" >Обновите профиль</Button>
+                        </Form.Item>
+                    </Form>
+                )}
+                
             </Card>
         </Layout>
     )
